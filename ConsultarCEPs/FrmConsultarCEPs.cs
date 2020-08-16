8c3e843209d482224f0484450dc10527a3c5d9a4
leobuck <leobuck12@gmail.com>
2020-08-16 18:08:26 -0300

    first commit

---
+++ ConsultarCEPs/FrmConsultarCEPs.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsultarCEPs
+{
+    public partial class FrmConsultarCEPs : Form
+    {
+        public FrmConsultarCEPs()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(txtCEP.Text))
+            {
+                using(var ws = new WSCorreios.AtendeClienteClient())
+                {
+                    try
+                    {
+                        var endereco = ws.consultaCEP(txtCEP.Text.Trim());
+
+                        txtEstado.Text = endereco.uf;
+                        txtCidade.Text = endereco.cidade;
+                        txtBairro.Text = endereco.bairro;
+                        txtRua.Text = endereco.end;
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Informe um CEP válido!", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtCEP.Text = String.Empty;
+            txtEstado.Text = String.Empty;
+            txtCidade.Text = String.Empty;
+            txtBairro.Text = String.Empty;
+            txtRua.Text = String.Empty;
+        }
+    }
+}
